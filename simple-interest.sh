6b2a1803bc9cb86415a687696042321da6e65f2c
KARTHIK A K <karthikak1817@gmail.com>
2025-05-30 08:48:49 +0530

    Pull Request Phase

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,2 +1,2 @@
 # Calculating the simple interest for the organisation
-# we have considered this as an testing and no code changes has been done
+# we have considered this as an testing and no code changes has been done - bug-fix-typo branch
